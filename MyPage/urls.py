fdc7ce1be4fe661704b539ae0556b81743adc0b9
YourAlly <Royettemiranda@YAHOO.COM>
2020-07-02 05:03:32 +0800

    Latest Stable 02/07/2020

--- MyPage/urls.py
+++ MyPage/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+from . import views as my_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index, name='my-index')
+    path('', my_views.index, name='my-index'),
+    path('register/', my_views.register, name='my-register'),
+    path('profile/', my_views.profile, name='my-profile'),
+    path('login/', auth_views.LoginView.as_view(template_name="MyPage/login.html"), name='my-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="MyPage/logout.html"), name='my-logout')
 ]
--- MyPage/views.py
+++ MyPage/views.py
@@ -1,10 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from MyPage.forms import RegistrationForm
+from django.contrib import messages
 
+from django.contrib.auth.models import User
+from .models import Post, Comment, Message
 
 # Create your views here.
 
 def index(request):
     context = {
-        'page_title': "Home"
+        'page_title': "Home",
+        'posts': Post.objects.all()
     }
     return render(request, 'MyPage/index.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration Success!")
+            return redirect('my-login')
+
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'page_title': "Register",
+        'form': form
+    }
+    return render(request, 'MyPage/register.html', context)
+
+
+def profile(request):
+
+    context = {
+        'page_title': "My Profile",
+    }
+    return render(request, 'MyPage/profile.html', context)
