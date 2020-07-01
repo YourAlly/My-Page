3c68539e1a4e0afc0e1ad373fc9d8b00ce1179fe
YourAlly <Royettemiranda@yahoo.com>
2020-07-01 10:03:32 +0800

    Added Models

--- MyPage/admin.py
+++ MyPage/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-from .models import User, Message, Post
+from .models import Message, Post, Comment
 # Register your models here.
+admin.site.register(Message)
+admin.site.register(Post)
+admin.site.register(Comment)
+
--- MyPage/models.py
+++ MyPage/models.py
@@ -8,10 +8,11 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="written_posts")
     time_posted = models.DateTimeField(default=timezone.now)
+    
     content = models.TextField()
 
     def __str__(self):
-        return f"{author}: {time_posted}"
+        return f"{self.author}: {self.time_posted}"
 
 
 class Comment(models.Model):
@@ -20,19 +21,21 @@ class Comment(models.Model):
     on_post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="post_comments")
     time_commented = models.DateTimeField(default=timezone.now)
+    
     content = models.TextField()
 
     def __str__(self):
-        return f"{commenter}: {time_commented}"
+        return f"{self.commenter}: {self.time_commented}"
 
 
 class Message(models.Model):
-    by = models.ForeignKey(User, on_delete=models.CASCADE,
+    sent_by = models.ForeignKey(User, on_delete=models.CASCADE,
                            related_name='sent_messages')
-    to = models.ForeignKey(User, on_delete=models.CASCADE,
+    sent_to = models.ForeignKey(User, on_delete=models.CASCADE,
                            related_name='recieved_messages')
     time_sent = models.DateTimeField(default=timezone.now)
+
     content = models.TextField()
 
     def __str__(self):
-        return f"{time_sent}: From {by} to {to}"
+        return f"{self.time_sent}: From {self.sent_by} to {self.sent_to}"
